9a2e6e0cb4958fc78dcdf2dd9998fb06043f7440
leonkoech <leonkipkip@gmail.com>
2020-04-06 19:23:13 +0300

    push initial commit https://github.com/leonkoech/Easygit

--- easygit/easygit.py
+++ easygit/easygit.py
@@ -215,7 +215,7 @@ def push():
     os.system('git commit -m '+'"'+commitname+'"')
     # Commits the tracked changes and prepares them to be pushed to a remote repository.
     # To remove this commit and modify the file, use 'git reset --soft HEAD~1' and commit and add the file again.
-    os.system('git remote add origin'+gitlink)
+    os.system('git remote add origin '+gitlink)
     # Sets the new remote
 
     # reverse the array first
